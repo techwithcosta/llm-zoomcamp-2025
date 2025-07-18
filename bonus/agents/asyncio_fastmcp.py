d114e55378c76cba99145ff2af85e978db4dff36
Diogo Costa <techwithcosta@gmail.com>
2025-07-18 20:37:22 +0100

    added asyncio_fastmcp.py

---
+++ bonus/agents/asyncio_fastmcp.py
@@ -0,0 +1,16 @@
+# asyncio_fastmcp.py
+
+import asyncio
+from fastmcp import Client
+
+async def main():
+    async with Client("bonus/agents/weather_server.py") as mcp_client:
+        tools = await mcp_client.list_tools()
+        print(f"Available tools: {tools}")
+
+if __name__ == "__main__":
+    test = asyncio.run(main())
+
+
+# Output:
+# Available tools: [Tool(name='get_weather', title=None, description='Retrieves the temperature for a specified city.\n\nParameters:\n    city (str): The name of the city for which to retrieve weather data.\n\nReturns:\n    float: The temperature associated with the city.', inputSchema={'properties': {'city': {'title': 'City', 'type': 'string'}}, 'required': ['city'], 'type': 'object'}, outputSchema={'properties': {'result': {'title': 'Result', 'type': 'number'}}, 'required': ['result'], 'title': '_WrappedResult', 'type': 'object', 'x-fastmcp-wrap-result': True}, annotations=None, meta=None), Tool(name='set_weather', title=None, description="Sets the temperature for a specified city.\n\nParameters:\n    city (str): The name of the city for which to set the weather data.\n    temp (float): The temperature to associate with the city.\n\nReturns:\n    str: A confirmation string 'OK' indicating successful update.", inputSchema={'properties': {'city': {'title': 'City', 'type': 'string'}, 'temp': {'title': 'Temp', 'type': 'number'}}, 'required': ['city', 'temp'], 'type': 'object'}, outputSchema=None, annotations=None, meta=None)]
